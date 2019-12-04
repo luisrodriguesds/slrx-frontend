1cac814f79e59813e06c71495694289222e0f0e2
Luis Rodrigues <luisitaloar@gmail.com>
2019-12-04 13:55:33 -0300

    Mostrar grau de inicio e fim das amostrad DRX

--- src/pages/solicitations/solicitations.jsx
+++ src/pages/solicitations/solicitations.jsx
@@ -342,7 +342,8 @@ export default class solicitations extends React.Component {
                                      </div>
 			                      </td>
 			                      <td className="weight">
-			                      	<Link to={`/solicitacoes/ver-amostra/${solicitation.name}`}>{solicitation.name}</Link>
+			                      	<Link to={`/solicitacoes/ver-amostra/${solicitation.name}`}>{solicitation.name}</Link> <br/>
+									{(this.state.user.permission == true && solicitation.method == 'DRX') && <small>{solicitation.settings.dois_theta_inicial} a {solicitation.settings.dois_theta_final}</small> }
 									{/* <ModalSolicitation solicitation={solicitation} user={this.state.user}>{solicitation.name}</ModalSolicitation> */}
 								  </td>
 			                      <td className="align-middle">{solicitation.equipment == null ? '' : solicitation.equipment.name}</td>
