c257a073f56d819c7a82334eb76be41a16a9bc4a
Luis Rodrigues <luisitaloar@gmail.com>
2020-02-07 16:13:49 -0300

    Adicionando orientações na pagina de cadastro

--- src/pages/mainPage/Choose.jsx
+++ src/pages/mainPage/Choose.jsx
@@ -22,15 +22,38 @@ const Choose = () => {
                             </div>
                             <div className="card-body">
                                 <p>Escolha o seu tipo de Usuário:</p>
+                                
+                                <h3>Alunos</h3>
                                 <p className="text-danger">*Alunos deverão passar pela aprovação do seu professor/orientador</p>
-                                <p className="text-danger">*Professores deverão passar pela aprovação do responsável pelo Laboratório, Professor Sasaki.</p>
-                                <p className="text-danger">*Representantes de empresas podem cadastrar suas amostras, mas somente serão liberadas mediante a aprovação de uma proposta.</p>
-                                <p className="text-danger">*Usuários autônomos podem cadastrar suas amostras, mas somente serão liberadas mediante a aprovação de uma proposta.</p>
-                                <p className="text-danger">*Operadores somente serão liberados após a confirmação do responsável pelo Laboratório, Professor Sasaki.</p>
+                                <p className="text-danger">*Só poderá se cadastrar como aluno se estiver cursando <strong>graduação, mestrado, doutorado ou pós-doutorado</strong></p>
                                 <Link to="/cadastro/aluno" className="btn btn-primary btn-block">Aluno</Link>
+                               <hr/>
+                               <br/>
+                                <h3>Professor</h3>
+                                <p className="text-danger">*Professores deverão passar pela aprovação do responsável pelo Laboratório, Professor Sasaki.</p>
+                                <p className="text-danger">*Só poderá se cadastrar como professor se e somente se <strong>não</strong> estiver realizando <strong>mestrado, doutorado ou pós</strong>.</p>
+                                <p className="text-danger">*Só poderá se cadastrar como professor se for de <strong>Instituição de Ensino Superior (IES)</strong></p>
                                 <Link to="/cadastro/professor" className="btn btn-primary btn-block">Professor</Link>
+                                <hr/>
+                                <br/>
+
+                                <h3>Empresa</h3>
+                                <p className="text-danger">*Representantes de empresas podem cadastrar suas amostras, mas somente serão liberadas mediante a aprovação de uma proposta.</p>
+
                                 <Link to="/cadastro/empresa" className="btn btn-primary btn-block">Empresa</Link>
+                               <hr/>
+                                <br/>
+
+                                <h3>Autônomo</h3>
+                                <p className="text-danger">*Usuários autônomos podem cadastrar suas amostras, mas somente serão liberadas mediante a aprovação de uma proposta.</p>
+
                                 <Link to="/cadastro/autonomo" className="btn btn-primary btn-block">Autônomo</Link>
+                                
+                               <hr/>
+                                <br/>
+                                <h3>Operador</h3>
+                                <p className="text-danger">*Operadores somente serão liberados após a confirmação do responsável pelo Laboratório, Professor Sasaki.</p>
+
                                 <Link to="/cadastro/operador" className="btn btn-primary btn-block">Operador</Link>
                             </div>
                         </div>
